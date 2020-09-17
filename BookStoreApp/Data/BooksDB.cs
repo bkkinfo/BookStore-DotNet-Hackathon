1bdb87e480df9c99d48dfc803e96cd52ce136959
Bishnu Kashyap <bishnu.kashyap@tcs.com>
2020-09-17 11:48:24 +0530

    first commit

---
+++ BookStoreApp/Data/BooksDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using BookStoreApp.Models;
+
+namespace BookStoreApp.Data
+{
+    public class BooksDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=BookStoreApp;Integrated Security=True");
+        }
+    }
+}
